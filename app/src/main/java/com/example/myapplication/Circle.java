b12ff419204cd33c12b64d9797e8c09d0c21818d
Zhihao Qian <zqian3@ualberta.ca>
2024-09-26 21:15:06 -0600

    Fixed Circle.java with correct implementation

--- app/src/main/java/com/example/myapplication/Circle.java
+++ app/src/main/java/com/example/myapplication/Circle.java
@@ -1,7 +1,23 @@
 package com.example.myapplication;
 
 public class Circle extends Shape {
-    public Circle(int x, int y) {
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
         super(x, y);
+        this.radius = radius;
+        this.color = "green"; 
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
     }
 }
