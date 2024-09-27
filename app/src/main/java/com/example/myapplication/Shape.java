ada693790b81e2e3dd95b34d7c627a01d810ccdf
Zhihao Qian <zqian3@ualberta.ca>
2024-09-26 21:20:23 -0600

    Added color field and methods to Shape.java

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,12 +3,18 @@ package com.example.myapplication;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "green";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    private String color = "$1PUT_YOUR_CHOICE_OF_COLOR1";
+    public String getColor() {
+        return color;
+    }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
