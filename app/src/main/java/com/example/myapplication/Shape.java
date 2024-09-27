1fff643e93ad5a6c486a1034ea2739789041c4e2
bauga27 <gbautist@ualberta.ca>
2024-09-26 18:25:17 -0600

    yellow

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -3,10 +3,13 @@ package com.example.myapplication;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
+
 
 }
