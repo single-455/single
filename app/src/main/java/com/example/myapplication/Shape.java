2cadaacfed96f3785ed978690d658db545bc2fd8
wbsxAllen <wbsxallen@gmail.com>
2024-09-26 18:12:10 -0600

    update

---
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
